be6db4b539c110c2509c72383d41d271ec34f9db
Felipe Uliana <v.uliana@gmail.com>
2016-01-23 18:18:11 -0200

    Quiz completed: 'Biography Quiz'

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = 'Felipe Uliana';
-var role = 'Web Developer & Designer';
+var bio = {
+  contacts: {
+    mobile: '11 9.7034-3975',
+    email: 'v.uliana@gmail.com',
+    github: 'felipeuliana',
+  },
+  name: 'Felipe Uliana',
+  bioPic: 'images/me.jpg  ',
+  role: 'Web Developer & Designer',
+  skills: ['Web Developer', 'Web Design'],
+  welcomeMessage: 'Hi there!'
 
-var formattedName = HTMLheaderName.replace('%data%', name);
-var formattedRole = HTMLheaderRole.replace('%data%', role);
+};
 
-$('#header').prepend(formattedRole).prepend(formattedName);
+var formattedName = HTMLheaderName.replace('%data%', bio.name);
+var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
