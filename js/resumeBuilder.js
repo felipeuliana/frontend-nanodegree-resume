0c7e2e77942ee850e7d76c89fed235ff7e18a0ec
Felipe Uliana <v.uliana@gmail.com>
2016-01-23 16:27:33 -0200

    Quiz completed: 'Format Data, Build a Resume Header'

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $('#main').append('Felipe Uliana');
+ var formattedName = HTMLheaderName.replace('%data%', 'Felipe Uliana');
+ var formattedRole = HTMLheaderRole.replace('%data%', 'Web Developer & Designer');
+
+ $('#header').prepend(formattedName).append(formattedRole);
