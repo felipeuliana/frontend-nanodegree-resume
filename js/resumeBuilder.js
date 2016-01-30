99c6e786e4d4eb2c71ad53ea10a39dd532ec96bb
Felipe Uliana <v.uliana@gmail.com>
2016-01-30 17:30:19 -0200

    Quiz completed: 'For-In Loops'

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,6 +9,13 @@ var work = {
       "location": "St. André",
       "dates": "2014-2016",
       "description": "Web Designer at CVC"
+    },
+    {
+      "employer": "Top Web Sites",
+      "title": "Web Designer",
+      "location": "Lapa",
+      "dates": "2012-2014",
+      "description": "Web Designer at Top Web Sites"
     }
   ]
 };
@@ -68,9 +75,17 @@ var education = {
 var placeHolderString = '%data%';
 
 var formattedName = HTMLheaderName.replace(placeHolderString, 'Felipe Uliana');
-var formattedRole = HTMLheaderRole.replace(placeHolderString, 'Web Develope & Designer');
+var formattedRole = HTMLheaderRole.replace(placeHolderString, 'Web Developer & Designer');
+var formattedMobile = HTMLmobile.replace(placeHolderString, '11 9.7034-3975');
+var formattedEmail = HTMLemail.replace(placeHolderString, 'v.uliana@gmail.com');
+var formattedGithub = HTMLgithub.replace(placeHolderString, 'felipeuliana');
+var formattedLocation = HTMLlocation.replace(placeHolderString, 'São Paulo, Brasil');
+var formattedPic = HTMLbioPic.replace(placeHolderString, 'images/me.jpg');
 
-$('#header').append(formattedName, formattedRole);
+$('#header').prepend(formattedName, formattedRole);
+$('#header').append(formattedPic);
+
+$('#topContacts').append(formattedMobile , formattedEmail , formattedGithub, formattedLocation);
 
 if (bio.skills.length) {
   $('#header').append(HTMLskillsStart);
@@ -78,4 +93,13 @@ if (bio.skills.length) {
   $('#skills').append(formattedSkill);
   formattedSkill = HTMLskills.replace(placeHolderString, bio.skills[1]);
   $('#skills').append(formattedSkill);
+};
+
+for (job in work.jobs) {
+  if (work.jobs.hasOwnProperty(job)) {
+    $('#workExperience').append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace(placeHolderString, work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace(placeHolderString, work.jobs[job].title);
+    $('.work-entry:last').append(formattedEmployer + formattedTitle);
+  };
 };
