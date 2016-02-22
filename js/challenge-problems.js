67105c6e520b1bb25cb18f4f514255eb01efc86b
Felipe Uliana <v.uliana@gmail.com>
2016-02-21 21:09:14 -0300

    Quiz completed: 'Astronaut Arrays'

--- js/challenge-problems.js
+++ js/challenge-problems.js
@@ -23,11 +23,56 @@ function getRelationship(argA, argB) {
   }
 }
 
-// console.log(getRelationship(1,4));
-// console.log(getRelationship(1,1));
-// console.log(getRelationship("that",2));
-// console.log(getRelationship("this","something else"));
-// console.log(getRelationship(3));
-console.log(getRelationship("hi"));
+console.log(getRelationship(1,4));
+console.log(getRelationship(1,1));
+console.log(getRelationship('that',2));
+console.log(getRelationship('this','something else'));
+console.log(getRelationship(3));
+console.log(getRelationship('hi'));
 console.log(getRelationship(NaN));
 console.log(getRelationship(NaN, undefined));
+
+ moonWalkers = [
+  'Neil Armstrong',
+  'Buzz Aldrin',
+  'Pete Conrad',
+  'Alan Bean',
+  'Alan Shepard',
+  'Edgar Mitchell',
+  'David Scott',
+  'James Irwin',
+  'John Young',
+  'Charles Duke',
+  'Eugene Cernan',
+  'Harrison Schmitt'
+];
+
+function alphabetizer(names) {
+  // Your code goes here!
+  var i = 0;
+  var namesLength = names.length;
+  var splitedName = [];
+  var newName = {};
+  var newNames = [];
+
+  for (i; i < namesLength; i++) {
+    splitedName = names[i].split(' ');
+    newName = {
+      firstName: splitedName[0].toLowerCase(),
+      lastName: splitedName[1].toLowerCase(),
+      fullName: splitedName[1] + ', ' + splitedName[0]
+    };
+    newNames.push(newName);
+  }
+
+  newNames.sort(function(a, b) {
+    if (a.lastName < b.lastName) return -1;
+    if (a.lastName > b.lastName) return 1;
+    return 0;
+  });
+
+  return newNames;
+}
+
+// Try logging your results to test your code!
+console.log(alphabetizer(moonWalkers));
